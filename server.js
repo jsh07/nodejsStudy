3afaae216139ddb8d0f470f12a36449f531e5bb6
lynx <coeducation97@gmail.com>
2024-01-22 20:12:08 +0900

    ejs 라이브러리 사용해보기

--- server.js
+++ server.js
@@ -5,33 +5,36 @@ const app = express()
 // static 파일을 html에서 쓰고 싶을 때 해당 파일들이 들어 있는 폴더(public) 등록
 app.use(express.static(__dirname + '/public'));
 
+// ejs 세팅
+app.set('view engine', 'ejs');
+
 // DB 연결
-const { MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 let db;
 const url = 'mongodb+srv://admin:qwer1234@lynx.fenevp1.mongodb.net/?retryWrites=true&w=majority';
-new MongoClient(url).connect().then((client)=>{
+new MongoClient(url).connect().then((client) => {
     console.log('DB연결성공');
     db = client.db('forum');
 
     // 서버 띄우는 코드
     // 8080: Port번호
-    app.listen(8080, ()=>{
+    app.listen(8080, () => {
         console.log('http://localhost:8080 에서 서버 실행중');
     });
 
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
 
 
-
-
-
 // __dirname : 현재 프로젝트의 절대 경로 (server.js 담긴 폴더)
-app.get('/', (요청, 응답)=>{
+app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html');
 });
-app.get('/insert', (요청, 응답)=>{
-    db.collection('post').insertOne({title: '123'});
+
+app.get('/list', async (요청, 응답) => {
+    // post 컬렉션의 모든 document 출력 - 외워
+    let result = await db.collection('post').find().toArray();
+    응답.render('list.ejs', {list: result});
 });
