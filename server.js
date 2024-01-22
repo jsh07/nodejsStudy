f04c60dbae26ae4db9ffe01eb7ce7ca1b3ab3cf4
lynx <coeducation97@gmail.com>
2024-01-22 15:34:01 +0900

    웹페이지 보내주기(라우팅)

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ app.listen(8080, ()=>{
     console.log('http://localhost:8080 에서 서버 실행중');
 });
 
-// 간단한 서버 기능
+// __dirname : 현재 프로젝트의 절대 경로 (server.js 담긴 폴더)
 app.get('/', (요청, 응답)=>{
-    응답.send('반갑다');
-})
+    응답.sendFile(__dirname + '/index.html');
+});
+
+// 숙제
+// 누가 /about 으로 접속하면 내 소개용 html 페이지를 보내주자.
+app.get('/about', (요청, 응답)=>{
+    응답.sendFile(__dirname + '/about.html');
+});
